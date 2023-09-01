a93703319181507af1c449988ebe16cb922decdf
Felipe <felipemouradev@gmail.com>
2023-08-31 23:37:45 -0500

    just some comments

--- helpers.py
+++ helpers.py
@@ -1,8 +1,10 @@
+""" Helper functions for the application """
 import os
 import dotenv
 
 dotenv.load_dotenv()
 
 def get_env(key):
+	""" Get environment variable """
 	return os.environ.get(key)
 
